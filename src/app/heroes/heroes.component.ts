546ba9f51b5df060634b51805ac192d3444b0938
Vysakh-T <53861783+Vysakh-T@users.noreply.github.com>
2020-11-21 16:03:14 +0530

    Class bindings and ng directives

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/app/hero';
+import { HEROES } from '../mock-heroes'
 
 @Component({
   selector: 'app-heroes',
@@ -8,9 +9,10 @@ import { Hero } from 'src/app/hero';
 })
 export class HeroesComponent implements OnInit {
   
-  hero : Hero = {
-    id : 1,
-    name : 'Windstorm'
+  heroes = HEROES;
+  selectedHero : Hero;
+  onSelect(hero : Hero) : void{
+    this.selectedHero = hero;
   }
 
   constructor() { }
